d7291f3cb45fd7838e8cfdb650800e04c261d181
oyebayofemi <oyebayooluwafemi@gmail.com>
2023-09-22 14:32:45 -0400

    Initial Commit

---
+++ IntoToSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IntoToSwiftUI
+//
+//  Created by oyebayo oluwafemi on 2023-09-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IntoToSwiftUI/IntoToSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntoToSwiftUIApp.swift
+//  IntoToSwiftUI
+//
+//  Created by oyebayo oluwafemi on 2023-09-22.
+//
+
+import SwiftUI
+
+@main
+struct IntoToSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
