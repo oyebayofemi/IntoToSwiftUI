98b0f28d476a8f8a567ec2179718763165870ba5
oyebayofemi <oyebayooluwafemi@gmail.com>
2024-07-04 19:19:58 -0400

    init

---
+++ IntoToSwiftUI/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  IntoToSwiftUI
+//
+//  Created by oyebayo oluwafemi on 2023-09-22.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}
